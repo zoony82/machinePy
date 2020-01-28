a649c1482f175d267fb8e3c030ee0234b297cd49
jjh <zoony82@gmail.com>
2020-01-28 20:51:49 +0900

    Part3. model test

--- kaggle/Titanic01_EDAToPrediction.py
+++ kaggle/Titanic01_EDAToPrediction.py
@@ -28,8 +28,8 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 plt.style.use('fivethirtyeight')
-
-data = pd.read_csv('C:\\04_dataset\\titanic\\train.csv')
+data = pd.read_csv('/home/jjh/문서/dataset/titanic/train.csv')
+# data = pd.read_csv('C:\\04_dataset\\titanic\\train.csv')
 data.head()
 
 # checking for total null values
@@ -368,11 +368,29 @@ While making or training models, we should try to eliminate redundant features a
 Now from the above heatmap,we can see that the features are not much correlated. The highest correlation is between SibSp and Parch i.e 0.41. So we can carry on with all features.
 '''
 
-#todo : 2020.01.23 할차례
 
 # Part2. Feature Engineering and Data Cleaning
+'''
+Now what is Feature Engineering?
+Whenever we are given a dataset with features, it is not necessary that all the features will be important. 
+There maybe be many redundant features which should be eliminated. 
+Also we can get or add new features by observing or extracting information from other features.
+An example would be getting the Initials feature using the Name Feature. 
+Lets see if we can get any new features and eliminate a few. 
+Also we will transform the existing relevant features to suitable form for Predictive Modeling.
+'''
 
-# continous values into categorical values
+'''
+Age_band
+Problem With Age Feature:
+As I have mentioned earlier that Age is a continous feature, there is a problem with Continous Variables in Machine Learning Models.
+Eg:If I say to group or arrange Sports Person by Sex, We can easily segregate them by Male and Female.
+Now if I say to group them by their Age, then how would you do it? 
+If there are 30 Persons, there may be 30 age values. Now this is problematic.
+We need to convert these continous values into categorical values by either Binning or Normalisation. 
+I will be using binning i.e group a range of ages into a single bin or assign them a single value.
+Okay so the maximum age of a passenger was 80. So lets divide the range from 0-80 into 5 bins. So 80/5=16. So bins of size 16.
+'''
 
 data['Age']
 data.loc[data['Age']<=16,'Age_band']=0
@@ -381,7 +399,7 @@ data.loc[(data['Age']>32)&(data['Age']<=48),'Age_band']=2
 data.loc[(data['Age']>48)&(data['Age']<=64),'Age_band']=3
 data.loc[data['Age']>64,'Age_band']=4
 
-data['Age_band'].value_counts().to_frame().style.background_gradient(cmap='summer')
+data['Age_band'].value_counts().to_frame()
 
 # statistics pclass & age to survived
 sns.factorplot(x='Age_band',y='Survived',data=data, col='Pclass')
@@ -417,3 +435,217 @@ data.loc[(data['Fare']>14.454)&(data['Fare']<=31),'Fare_cat']=2
 data.loc[(data['Fare']>31)&(data['Fare']<=513),'Fare_cat']=3
 
 sns.factorplot('Fare_cat','Survived',data=data,hue='Sex')
+'''
+Clearly, as the Fare_cat increases, the survival chances increases. 
+This feature may become an important feature during modeling along with the Sex.
+'''
+
+
+'''
+Converting String Values into Numeric
+Since we cannot pass strings to a machine learning model, 
+we need to convert features loke Sex, Embarked, etc into numeric values.
+'''
+
+data['Sex'].replace(['male','female'],[0,1],inplace=True)
+data['Embarked'].replace(['S','C','Q'],[0,1,2],inplace=True)
+data['Initial'].replace(['Mr','Mrs','Miss','Master','Other'],[0,1,2,3,4],inplace=True)
+
+'''
+Dropping UnNeeded Features
+Name--> We don't need name feature as it cannot be converted into any categorical value.
+Age--> We have the Age_band feature, so no need of this.
+Ticket--> It is any random string that cannot be categorised.
+Fare--> We have the Fare_cat feature, so unneeded
+Cabin--> A lot of NaN values and also many passengers have multiple cabins. So this is a useless feature.
+Fare_Range--> We have the fare_cat feature.
+PassengerId--> Cannot be categorised.
+'''
+
+data.drop(['Name','Age','Ticket','Fare','Cabin','Fare_range','PassengerId'],axis=1,inplace=True)
+sns.heatmap(data.corr(),annot=True,cmap='RdYlGn',linewidths=0.2,annot_kws={'size':20})
+'''
+Now the above correlation plot, we can see some positively related features. 
+Some of them being SibSp andd Family_Size and Parch and Family_Size and some negative ones like Alone and Family_Size.
+'''
+
+data.to_csv('/home/jjh/문서/dataset/titanic/data_clean.csv',index=False)
+
+# Part3: Predictive Modeling
+data_clean = pd.read_csv('/home/jjh/문서/dataset/titanic/data_clean.csv')
+
+'''
+We have gained some insights from the EDA part. 
+But with that, we cannot accurately predict or tell whether a passenger will survive or die. 
+So now we will predict the whether the Passenger will survive or not using some great Classification Algorithms.
+Following are the algorithms I will use to make the model:
+1)Logistic Regression
+2)Support Vector Machines(Linear and radial)
+3)Random Forest
+4)K-Nearest Neighbours
+5)Naive Bayes
+6)Decision Tree
+7)Logistic Regression
+'''
+
+from sklearn.linear_model import LogisticRegression
+from sklearn import svm
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn import metrics #accuracy measure
+from sklearn.metrics import confusion_matrix #for confusion matrix
+
+train, test = train_test_split(data_clean, test_size=0.3, random_state=0, stratify=data['Survived'])
+# stratify : 지정한 데이터의 비율 유지
+train_x=train[train.columns[1:]]
+train_y=train[train.columns[:1]]
+test_x=test[test.columns[1:]]
+test_y=test[test.columns[:1]]
+x=data[data.columns[1:]]
+y=data[data.columns[:1]]
+
+#Radial Support Vector Machines : rbf-SVM
+model = svm.SVC(kernel='rbf', C=1, gamma=0.1)
+model.fit(train_x,train_y)
+pred1 = model.predict(test_x)
+print(metrics.accuracy_score(pred1,test_y))
+#0.835
+
+#Linear Support Vector Machines : linear-SVM
+model = svm.SVC(kernel='linear', C=0.1, gamma=0.1)
+model.fit(train_x,train_y)
+pred2 = model.predict(test_x)
+print(metrics.accuracy_score(pred2,test_y))
+#0.817
+
+#Logistic Regression
+model = LogisticRegression()
+model.fit(train_x,train_y)
+pred3 = model.predict(test_x)
+print(metrics.accuracy_score(pred3,test_y))
+#0.817
+
+#Decision TRee
+model = DecisionTreeClassifier()
+model.fit(train_x,train_y)
+pred4 = model.predict(test_x)
+print(metrics.accuracy_score(pred4,test_y))
+#0.809
+
+#K-Nearest Neighbors(KNN)
+model = KNeighborsClassifier()
+model.fit(train_x,train_y)
+pred5 = model.predict(test_x)
+print(metrics.accuracy_score(pred5,test_y))
+#0.832
+'''
+Now the accuracy for the KNN model changes as we change the values for n_neighbours attribute. 
+The default value is 5. Lets check the accuracies over various values of n_neighbours.
+'''
+a_index = list(range(1,11))
+a = pd.Series()
+x = [0,1,2,3,4,5,6,7,8,9,10]
+
+for i in list(range(1,11)):
+    model = KNeighborsClassifier(n_neighbors=i)
+    model.fit(train_x,train_y)
+    pred = model.predict(test_x)
+    a = a.append(pd.Series(metrics.accuracy_score(pred,test_y)))
+
+plt.plot(a_index,a)
+plt.xticks(x)
+fig.plt.gcf()
+fig.set_size_inches(12,6)
+plt.show()
+
+#Gussian Naive Bayes
+model = GaussianNB()
+model.fit(train_x,train_y)
+pred6 = model.predict(test_x)
+print(metrics.accuracy_score(pred6,test_y))
+#0.813
+
+
+#Random Forests
+model = RandomForestClassifier(n_estimators=100)
+model.fit(train_x,train_y)
+pred7 = model.predict(test_x)
+print(metrics.accuracy_score(pred7,test_y))
+#0.817
+
+'''
+The accuracy of a model is not the only factor that determines the robustness of the classifier. 
+Let's say that a classifier is trained over a training data and tested over the test data and it scores an accuracy of 90%.
+Now this seems to be very good accuracy for a classifier, but can we confirm that it will be 90% for all the new test sets that come over??. 
+The answer is No, because we can't determine which all instances will the classifier will use to train itself. 
+As the training and testing data changes, the accuracy will also change. 
+It may increase or decrease. This is known as model variance.
+
+To overcome this and get a generalized model,we use Cross Validation.
+'''
+
+
+'''
+Cross Validation
+Many a times, the data is imbalanced, i.e there may be a high number of class1 instances but less number of other class instances. 
+Thus we should train and test our algorithm on each and every instance of the dataset. 
+Then we can take an average of all the noted accuracies over the dataset.
+
+1)The K-Fold Cross Validation works by first dividing the dataset into k-subsets.
+2)Let's say we divide the dataset into (k=5) parts. We reserve 1 part for testing and train the algorithm over the 4 parts.
+3)We continue the process by changing the testing part in each iteration and training the algorithm over the other parts. 
+The accuracies and errors are then averaged to get a average accuracy of the algorithm.
+This is called K-Fold Cross Validation.
+4)An algorithm may underfit over a dataset for some training data and sometimes also overfit the data for other training set. 
+Thus with cross-validation, we can achieve a generalised model.
+'''
+
+from sklearn.model_selection import KFold
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_val_predict
+kfold = KFold(n_splits=10, random_state=22)
+xyz=[]
+accuracy=[]
+std=[]
+classifiers = ['Linear Svm','Radial Svm','Logistic Regression','KNN','Decision Tree','Naive Bayes','Random Forest']
+models = [svm.SVC(kernel='linear'),svm.SVC(kernel='rbf'),LogisticRegression(),KNeighborsClassifier(n_neighbors=9),DecisionTreeClassifier(),GaussianNB(),RandomForestClassifier(n_estimators=100)]
+for i in models:
+    model = i
+    cv_result = cross_val_score(model,x,y,cv=kfold,scoring="accuracy")
+    cv_result = cv_result
+    xyz.append(cv_result.mean())
+    std.append(cv_result.std())
+    accuracy.append(cv_result)
+
+new_models_df2=pd.DataFrame({'CV Mean':xyz,'Std':std},index=classifiers)
+new_models_df2
+'''
+                      CV Mean       Std
+Linear Svm           0.793471  0.047797
+Radial Svm           0.828290  0.034427
+Logistic Regression  0.805843  0.021861
+KNN                  0.813783  0.041210
+Decision Tree        0.808127  0.029966
+Naive Bayes          0.801386  0.028999
+Random Forest        0.815955  0.035495
+'''
+
+plt.subplots(figsize=(12,6))
+box=pd.DataFrame(accuracy,index=[classifiers])
+box.T.boxplot()
+
+new_models_df2['CV Mean'].plot.barh(width=0.8)
+plt.title('Average CV Mean Accuracy')
+fig=plt.gcf()
+fig.set_size_inches(8,5)
+plt.show()
+
+'''
+The classification accuracy can be sometimes misleading due to imbalance. 
+We can get a summarized result with the help of confusion matrix, which shows where did the model go wrong, 
+or which class did the model predict wrong.
+'''
+
